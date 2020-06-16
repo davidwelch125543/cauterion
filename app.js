6510807a0e303bec02a32008cf68fcbec27a0cbe
David Welch <davidwelch125543@gmail.com>
2020-06-16 15:07:46 +0400

    Changed port number

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3001;
+const port = 3002;
 const server = require('http').createServer(app);
 const cors = require('cors');
 
