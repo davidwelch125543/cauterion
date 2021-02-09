9841a100725e959857bd06653045c5a29ccf6502
davidwelch125543 <davidwelch125543@github.com>
2021-02-09 10:39:38 +0400

    Member update by operator

--- routes/members.js
+++ routes/members.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { AuthorizerMiddleware } = require('../middlewares/middleware.authorizer');
 const membersController = require('../controllers/membersController');
+const { USER_TYPES } = require('../models/user.model');
 const UserAuthorizerMiddleware = AuthorizerMiddleware();
+const User_OperAuthorizerMiddleware = AuthorizerMiddleware([USER_TYPES.USER, USER_TYPES.OPERATOR]);
 
 // Family accounts
 router.post('/', UserAuthorizerMiddleware, membersController.createFamilyAccount);
 router.get('/', UserAuthorizerMiddleware, membersController.getMembersList);
 router.get('/:memberId', UserAuthorizerMiddleware, membersController.getMemberById);
-router.put('/:memberId', UserAuthorizerMiddleware, membersController.updateMember);
-
+router.put('/:memberId', User_OperAuthorizerMiddleware, membersController.updateMember);
 router.post('/account-request', UserAuthorizerMiddleware, membersController.requestForStandaloneAccount);
 
 module.exports = router;
